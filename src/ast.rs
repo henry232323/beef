c3fe6729ac4e395e13561cf042f1dce99f012a36
henry232323 <14969260+henry232323@users.noreply.github.com>
2022-07-06 23:21:39 -0700

    Initial commit

--- src/ast.rs
+++ src/ast.rs
@@ -1,18 +1,83 @@
-lalrpop_mod!(pub grammar); // synthesized by LALRPOP
+// pub enum Expr {
+//     Num(i32),
+//     BinaryOp(Box<Expr>, Opcode, Box<Expr>),
+//     Error,
+//     Str(String)
+// }
+
+use std::fmt::{Debug, Error, Formatter};
 
 pub enum Expr {
-    IntExpr(i64),
+    Boolean(bool),
+    Integer(i32),
+    Float(f32),
     BinaryOp(Box<Expr>, Opcode, Box<Expr>),
+    UnaryOp(Opcode, Box<Expr>),
+    Error,
+    Str(String),
+    List(Vec<Box<Expr>>),
+    Variable(String),
+    FunctionCall(Box<Expr>, Vec<Box<Expr>>),
+    ItemSubscription(Box<Expr>, Box<Expr>),
+    AttrAccess(Box<Expr>, String),
 }
 
+pub enum Statement {
+    Expression(Box<Expr>),
+    If(Box<Expr>, Vec<Box<Statement>>, Option<Vec<Box<Statement>>>),
+    Function(String, Box<Vec<String>>, Vec<Box<Statement>>),
+}
+
+
+#[derive(Copy, Clone)]
 pub enum Opcode {
     Mul,
     Div,
     Add,
     Sub,
+    Pow
 }
 
-struct IntExpr {
-    value: i64
+impl Debug for Expr {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::Expr::*;
+        match &*self {
+            Integer(n) => write!(fmt, "Integer({:#?})", n),
+            Float(n) => write!(fmt, "Float({:#?})", n),
+            Boolean(b) => write!(fmt, "Boolean({:#?})", b),
+            BinaryOp(ref l, op, ref r) => write!(fmt, "BinaryOp({:#?}, {:#?}, {:#?})", l, op, r),
+            UnaryOp(op, ref r) => write!(fmt, "UnaryOp({:#?}, {:#?})", op, r),
+            Str(s) => write!(fmt, "Str({:#?})", s),
+            List(l) => write!(fmt, "List({:#?})", l),
+            Variable(v) => write!(fmt, "Variable({:#})", v),
+            FunctionCall(ref fun, args) => write!(fmt, "FunctionCall({:#?}, args={:#?})", fun, args),
+            ItemSubscription(ref expr, ref index) => write!(fmt, "ItemSubscription({:#?}, index={:#?})", expr, index),
+            AttrAccess(ref expr, attr) => write!(fmt, "AttrAccess({:#?}, attr={:#?})", expr, attr),
+            Error => write!(fmt, "error"),
+        }
+    }
 }
 
+impl Debug for Statement {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::Statement::*;
+        match &*self {
+            Expression(ref expr) => write!(fmt, "Expression({:#?})", expr),
+            If(ref cond, body, elsebody) => write!(fmt, "If(cond=({:#?}), body={:#?} elsebody={:#?})", cond, body, elsebody),
+            Function(ref name, args, body) => write!(fmt, "Function(name=({:#?}), args={:#?} body={:#?})", name, args, body),
+        }
+    }
+}
+
+impl Debug for Opcode {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::Opcode::*;
+        match *self {
+            Mul => write!(fmt, "Op::Mul"),
+            Div => write!(fmt, "Op::Div"),
+            Add => write!(fmt, "Op::Add"),
+            Sub => write!(fmt, "Op::Sub"),
+            Pow => write!(fmt, "Op::Pow"),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+pub mod ast;
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub grammar); // synthesized by LALRPOP
+
+fn main() {
+    let result = grammar::BodyParser::new().parse(r#"
+    if 4 {
+         abcde;
+         [1,2,3,4] + 22 * 22 * 23 + 5 ** 3 + "str" - 36.2;
+    };
+    if 3 {
+        abcde;
+    } else {
+        cdef;
+    };
+    [1,2,3];
+    fun myfun (a,b,c) {
+        if 3 {
+            print(-"Hello world!");
+        };
+    };
+    fun myfun () {
+        if 3 {
+            print(+"Hello world!".myattr[1].funky());
+        };
+    };
+    "#);
+    println!("{:#?}", result)
+}
